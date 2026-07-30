d385c1fb2163ca37bbadef36d2bab806a478d435
arthur <arthur.antonelli76@gmail.com>
2026-07-29 22:17:59 -0300

    continuando calculadora (função clear, backspace)

--- lab_1/script.js
+++ lab_1/script.js
@@ -1,36 +1,98 @@
 const display = document.querySelector('#display')
-
 const teclado = document.querySelector('.keys')
 
 let entradaAtual = '0'
 let valorAnterior = null
 let operador = null
 
-
-
 teclado.addEventListener('click', (e) => {
     const botao = e.target
     if (!botao) return
-    
+
     const digito = botao.dataset.digit
     const operacao = botao.dataset.op
     const acao = botao.dataset.action
 
-    if (digito){
+    if (digito) {
         inserirDigito(digito)
+        atualizarDisplay(entradaAtual)
         return
     }
-    if(operacao){
+    if (operacao) {
         // registrarOperacao(operacao)
         return
     }
-    if(acao){
-        // executarAcao(acao)
+    if (acao) {
+        executarAcao(acao)
+        atualizarDisplay(entradaAtual)
         return
     }
 })
 
 const inserirDigito = digito => {
-    display.textContent += digito
+
+    if (digito == "." && entradaAtual.includes(".")) return
+
+    if (entradaAtual == "0") {
+        entradaAtual = digito
+        return
+    }
+
+    entradaAtual += digito
 }
 
+const atualizarDisplay = (entrada) => {
+    display.textContent = entrada
+}
+
+const registrarOperacao = (operacao) => {
+    if (operacao == "raiz" || operacao == "porcento") {
+        calcularUnaria(operacao)
+        return
+    }
+
+    calcularBinaria(operacao)
+}
+
+const calcularUnaria = (op) => {
+    
+}
+
+const calcularBinaria = (op) => {
+    valorAnterior = Number(entradaAtual)
+    operador = op
+    entradaAtual = "0"
+    
+}
+
+const executarAcao = acao => {
+    switch (acao) {
+        case "clear":
+            limparTudo()
+            break
+        case "backspace":
+            backspace()
+            break
+        case "sign":
+        case "equals":
+    }
+
+}
+
+const limparTudo = () => {
+    entradaAtual = 0;
+    valorAnterior = null;
+    operador = null;
+    atualizarDisplay(entradaAtual)
+}
+
+const backspace = () => {
+    // if(entradaAtual.length == "1"){
+    //     entradaAtual = 0;
+    //     return
+    // }
+    // entradaAtual = entradaAtual.slice(0, -1)
+
+    entradaAtual = entradaAtual.length > 1 ? entradaAtual.slice(0, -1) : entradaAtual = '0'
+    atualizarDisplay(entradaAtual)
+}
